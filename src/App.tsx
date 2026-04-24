764295ca605e65f3577f2fbb773eef6a880ac05d
Thalles <thallessalles042@gmail.com>
2026-04-23 21:47:59 -0300

    Criação da tela inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import Home from "./pages/home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>🚀 Vite React Template 🚀</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+  return <Home />;
 }
 
 export default App
