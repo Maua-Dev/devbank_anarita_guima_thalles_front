f3db9cfe4ca647d8bc26824c8aa5b80ebc57b955
guima <joaopedroguima1307@gmail.com>
2026-04-27 18:19:30 -0300

    fix: navbar stlying

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/navbar'
 function App() {
   return (
     <div>
+
       <Home />
     </div>
   );
--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -3,12 +3,12 @@ import user from '../../assets/user.svg';
 
 function Navbar({ userInfo = { name: "GuiGuerreiro", agency: "0069", acnumber: "12345-6" } }) {
     return (
-        <nav className="bg-[#0073E6] w-full">
+        <nav className="bg-[#0073E6] w-full h-24">
             <div className="flex items-center justify-between  mx-auto h-20">
-                <img className="w-auto h-12 px-2" src={logo} alt="Logo" />
+                <img className="w-auto h-22 px-8 mt-4" src={logo} alt="Logo" />
 
-                <div className="flex items-center space-x-4 ">
-                    <div className="flex flex-col items-left text-xs md:text-sm bg-white p-1 rounded-xl">
+                <div className="flex items-center space-x-4 mr-8">
+                    <div className=" w-46 h-20flex-col mt-4 items-left space-y-2 text-md md:text-sm bg-white p-1 rounded-xl">
                         <div className="text-[#0073E6]">
                             Nome: {userInfo.name}
                         </div>
@@ -19,7 +19,7 @@ function Navbar({ userInfo = { name: "GuiGuerreiro", agency: "0069", acnumber: "
                             Conta: {userInfo.acnumber}
                         </div>
                     </div>
-                    <img className="h-18 w-18 rounded-full bg-white p-1" src={user} alt="User" />
+                    <img className="mt-4 h-20 w-20 rounded-full bg-white p-1" src={user} alt="User" />
                 </div>
             </div>
         </nav>
